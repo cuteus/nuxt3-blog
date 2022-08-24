6e1b6b10badc446e58ba4624e946b9b693450d11
cuteus <97730167+cuteus@users.noreply.github.com>
2022-08-24 20:48:08 +0800

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,12 +1,12 @@
 export default {
-  title: "yun yu yuan", // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: "yunyuyuan", // github账户名
+  title: "homo的博客", // 网站标题
+  nickName: "homo之家", // 昵称
+  githubName: "cuteus", // github账户名
   githubRepo: "nuxt3-blog", // 勿修改
   githubBranch: "master", // 勿修改
   domain: "https://blog.halberd.cn", // rss域名
-  SEO_description: "云与原的博客，书写值得书写的东西",
-  SEO_keywords: "yunyuyuan,yunyuyuan's blog,云与原,云与原的博客",
+  SEO_description: "homo的博客，书写恶臭的文章",
+  SEO_keywords: "yunyuyuan,yunyuyuan's blog,114514，homo的博客",
 
   themeColor: "#2aa0bb", // 主题色
   CommentRepoId: "R_kgDOGRGGfg", // https://giscus.app/zh-CN
